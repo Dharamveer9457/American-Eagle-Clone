fb785f16c4b107cb323eb5274c400aa2c7882e01
Dharamveer <dharammveer945734@outlook.com>
2023-01-19 15:30:42 +0530

    day-2 work finished

---
+++ scripts/signUp.js
@@ -0,0 +1,27 @@
+let form =  document.querySelector("#signupform")
+
+  let register = document.querySelector(".registerbtn")
+
+let signupData = JSON.parse(localStorage.getItem("signup-data"))||[]
+
+register.addEventListener("click",function(event){
+
+    event.preventDefault()
+
+    let credential = {
+        firstname : form.firstname.value,
+        lastname : form.lastname.value,
+        email : form.email.value,
+        phone : form.phone.value,
+        password : form.password.value
+    }
+   if(firstname == "" || lastname=="" || email=="" || phone=="" || password==""){
+    alert("Please fill all the details")
+   }else{
+    signupData.push(credential)
+    localStorage.setItem("signup-data", JSON.stringify(signupData))
+    alert("Sign Up Successful")
+   }
+   
+})
+
